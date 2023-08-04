350d2b9edc1986474dc0b986b22991829789f379
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-04 17:35:58 +0300

    Make grid that stays 16 divs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    container.appendChild(row);
+
     for (let j = 0; j < 16; j++) {
         const divs = document.createElement('div');
-        divs.setAttribute('class', 'gridDivs')
-        container.appendChild(divs);
+        divs.setAttribute('class', 'gridDivs');
+        row.appendChild(divs); 
     }
+
+    
 }
